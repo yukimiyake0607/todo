278eae2efda5286a0dd950e3b44ad0e524477ea4
yukimiyake0607 <yukimiyake38@gmail.com>
2024-06-24 21:10:03 +0900

    add: tasks以外の子要素であるタイトルやアイコンの追加

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,10 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class TasksScreen extends StatelessWidget {
   const TasksScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      body: Container(
+        padding: const EdgeInsets.only(
+          top: 60.0,
+          right: 30.0,
+          bottom: 30.0,
+          left: 30.0,
+        ),
+        child: const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CircleAvatar(
+              backgroundColor: Colors.white,
+              radius: 30.0,
+              child: Icon(
+                Icons.list,
+                size: 30.0,
+                color: Colors.lightBlueAccent,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Todoey',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 50.0,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Text(
+              '12 Tasks',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
