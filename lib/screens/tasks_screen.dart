56c9b75b595ef4cb9e32b7e5bca3ea6667bc2fb9
yukimiyake0607 <yukimiyake38@gmail.com>
2024-06-24 22:15:22 +0900

    add: TasksListクラスを追加

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -9,47 +9,98 @@ class TasksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: Container(
-        padding: const EdgeInsets.only(
-          top: 60.0,
-          right: 30.0,
-          bottom: 30.0,
-          left: 30.0,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const CircleAvatar(
-              backgroundColor: Colors.white,
-              radius: 30.0,
-              child: Icon(
-                Icons.list,
-                size: 30.0,
-                color: Colors.lightBlueAccent,
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Text(
-              'Todoey',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 50.0,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            const Text(
-              '12 Tasks',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-              ),
-            ),
-            Container(),
-          ],
-        ),
+        onPressed: () {},
       ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(
+              top: 60.0,
+              right: 30.0,
+              bottom: 30.0,
+              left: 30.0,
+            ),
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                  child: Icon(
+                    Icons.list,
+                    size: 30.0,
+                    color: Colors.lightBlueAccent,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Todoey',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  '12 Tasks',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
+                ),
+              ),
+              child: TasksList(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TasksList extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      children: [
+        TaskTile(),
+        TaskTile(),
+        TaskTile(),
+        TaskTile(),
+      ],
+    );
+  }
+}
+
+class TaskTile extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text('Buy Milk'),
+      trailing: Checkbox(value: false, onChanged: null),
     );
   }
 }
