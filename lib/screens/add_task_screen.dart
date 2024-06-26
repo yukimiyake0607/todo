0d0fa7fbdb1021fa8bcf6b8e0758acc575f82708
yukimiyake0607 <yukimiyake38@gmail.com>
2024-06-26 22:26:49 +0900

    add: add_task_screenを追加
    refs タスクをリストに追加するための画面

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -1,10 +1,69 @@
 import 'package:flutter/material.dart';
 
 class AddTaskScreen extends StatelessWidget {
-  const AddTaskScreen({super.key});
+  AddTaskScreen({super.key});
+  String textField = '';
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      color: const Color(0xff757575),
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: 40.0,
+          vertical: 20.0,
+        ),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0),
+          ),
+        ),
+        child: Column(
+          children: [
+            const Text(
+              'Add Task',
+              style: TextStyle(
+                color: Colors.lightBlueAccent,
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'ここに入力',
+                enabledBorder: UnderlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Colors.lightBlueAccent, width: 3.0),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue, width: 4.0),
+                ),
+              ),
+              onChanged: (value) {
+                textField = value;
+              },
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
+            TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                  backgroundColor: Colors.lightBlueAccent,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.zero,
+                  ),
+                  minimumSize: Size(double.infinity, 50.0)),
+              child: const Text(
+                'Add',
+                style: TextStyle(color: Colors.white, fontSize: 20.0),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
