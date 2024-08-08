72cfd6ebb95ce3a3e8b8f2ff6c2985be0e00c96e
yukimiyake0607 <yukimiyake38@gmail.com>
2024-08-08 16:04:10 +0900

    add: showModalBottomSheetを追加
    refs HomeScreenのfloatingActionButtonに付与
    todoを追加するための画面である

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo/providers/tab_provider.dart';
+import 'package:todo/providers/todo_provider.dart';
 import 'package:todo/screens/completed_screen.dart';
 import 'package:todo/screens/todo_screen.dart';
 
@@ -15,6 +16,9 @@ class _HomeScreenState extends ConsumerState<HomeScreen>
     with SingleTickerProviderStateMixin {
   late final TabController _tabController =
       TabController(length: 2, vsync: this);
+
+  final _titleTextController = TextEditingController();
+  final _subtitleTextController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -39,6 +43,69 @@ class _HomeScreenState extends ConsumerState<HomeScreen>
           TodoScreen(),
           CompletedScreen(),
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blue,
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) {
+              return Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      width: 300,
+                      child: TextField(
+                        controller: _titleTextController,
+                        decoration: const InputDecoration(
+                          hintText: 'TODO',
+                          hintStyle: TextStyle(color: Colors.grey),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.blue, width: 2.0)),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 300,
+                      child: TextField(
+                        controller: _subtitleTextController,
+                        decoration: const InputDecoration(
+                          hintText: '内容',
+                          hintStyle: TextStyle(color: Colors.grey),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.blue, width: 2.0)),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: () {
+                        ref.read(todoProvider.notifier).addTodo(
+                            _titleTextController.text,
+                            _subtitleTextController.text);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                      ),
+                      child: const Text(
+                        '追加',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
       ),
     );
   }
