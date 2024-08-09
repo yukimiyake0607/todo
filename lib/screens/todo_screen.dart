d1eaa8c54c65e93ebbfa9b4e1248e69327fdf30c
yukimiyake0607 <yukimiyake38@gmail.com>
2024-08-09 23:16:35 +0900

    add: providerの情報を受け取れるようにAsyncValueを設定

--- lib/screens/todo_screen.dart
+++ lib/screens/todo_screen.dart
@@ -7,46 +7,54 @@ class TodoScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final todoList = ref.watch(todoProvider);
-    return ListView.builder(
-        itemCount: todoList.length,
-        itemBuilder: (context, index) {
-          final todo = todoList[index];
-          return Dismissible(
-            key: Key(todo.id),
-            background: Container(
-              color: Colors.red,
-              alignment: Alignment.centerRight,
-              padding: const EdgeInsets.all(20),
-              child: const Icon(
-                Icons.delete,
-                color: Colors.white,
+    final todoListAsync = ref.watch(todoProvider);
+    return todoListAsync.when(
+      data: (todoList) => ListView.builder(
+          itemCount: todoList.length,
+          itemBuilder: (context, index) {
+            final todo = todoList[index];
+            return Dismissible(
+              key: Key(todo.id),
+              background: Container(
+                color: Colors.red,
+                alignment: Alignment.centerRight,
+                padding: const EdgeInsets.all(20),
+                child: const Icon(
+                  Icons.delete,
+                  color: Colors.white,
+                ),
               ),
-            ),
-            onDismissed: (direction) {
-              ref.read(todoProvider.notifier).deleteTodo(todo.id);
-              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                content: Text('${todo.title} を削除しました'),
-                action: SnackBarAction(
-                    label: '元に戻す',
-                    onPressed: () {
-                      ref
-                          .read(todoProvider.notifier)
-                          .addTodo(todo.title, todo.subTitle);
-                    }),
-              ));
-            },
-            child: ListTile(
-              leading: Checkbox(
-                value: todo.isChecked,
-                onChanged: (value) {
-                  ref.read(todoProvider.notifier).toggleTodoChecked(todo.id);
-                },
+              onDismissed: (direction) {
+                ref.read(todoProvider.notifier).deleteTodo(todo.id);
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text('${todo.title} を削除しました'),
+                  action: SnackBarAction(
+                      label: '元に戻す',
+                      onPressed: () {
+                        ref
+                            .read(todoProvider.notifier)
+                            .addTodo(todo.title, todo.subTitle);
+                      }),
+                ));
+              },
+              child: ListTile(
+                leading: Checkbox(
+                  value: todo.isChecked,
+                  onChanged: (value) {
+                    ref.read(todoProvider.notifier).toggleTodoChecked(todo.id);
+                  },
+                ),
+                title: Text(todo.title),
+                subtitle: Text(todo.subTitle),
               ),
-              title: Text(todo.title),
-              subtitle: Text(todo.subTitle),
-            ),
-          );
-        });
+            );
+          }),
+      error: (error, stack) => Center(
+          child: Text(
+        'エラーが発生しました:$error',
+        style: Theme.of(context).textTheme.displaySmall,
+      )),
+      loading: () => const CircularProgressIndicator(),
+    );
   }
 }
