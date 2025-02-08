250fa5b409c00fa0da50f44f60d546e5e107e593
yukimiyake0607 <yukimiyake38@gmail.com>
2025-02-08 17:42:42 +0900

    add: Add Counter_App

---
+++ lib/ui/counter_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int count = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          '$count',
+          style: const TextStyle(fontSize: 50),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            count++;
+          });
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
