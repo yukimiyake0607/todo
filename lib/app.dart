42322a69af64d2b25dff0acdd717a5a1ee6a7907
yukimiyake0607 <yukimiyake38@gmail.com>
2025-02-23 17:00:44 +0900

    refactor: ファイルを移動

--- lib/app.dart
+++ lib/app.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:todo/ui/counter_screen.dart';
+import 'package:todo/presentation/pages/counter_screen.dart';
 
-class MainApp extends StatefulWidget {
+class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
-  @override
-  State<MainApp> createState() => _MainAppState();
-}
-
-class _MainAppState extends State<MainApp> {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
