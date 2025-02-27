c2ad9a8ae3110a7589d3e405afed22697551e68b
yukimiyake0607 <yukimiyake38@gmail.com>
2025-02-27 17:54:35 +0900

    update: bottom_navigationをWidgetで切り出し

---
+++ lib/presentation/bottom_navigation/bottom_navigation.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo/presentation/bottom_navigation/bottom_navigation_provider.dart';
+
+class BottomNavigation extends ConsumerWidget {
+  const BottomNavigation({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final bottomNavigationNum = ref.watch(bottomNavigationProvider);
+    return BottomNavigationBar(
+      currentIndex: bottomNavigationNum,
+      onTap: (value) {
+        ref.read(bottomNavigationProvider.notifier).state = value;
+      },
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.list_outlined),
+          label: 'リスト',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.check_box_outlined),
+          label: '完了',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_outline),
+          label: 'プロフィール',
+        )
+      ],
+    );
+  }
+}
