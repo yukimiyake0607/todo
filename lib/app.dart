13769e08c5026187696a0bc80880d702ad371352
yukimiyake0607 <yukimiyake38@gmail.com>
2025-02-27 17:56:52 +0900

    add: ThemeDataを追加

--- lib/app.dart
+++ lib/app.dart
@@ -6,8 +6,16 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
+    return MaterialApp(
+      home: const HomePage(),
+      theme: ThemeData(
+        primaryColor: const Color(0xFFF97316),
+        colorScheme: const ColorScheme.light(
+          primary: Color(0xFFF97316),
+          secondary: Color(0xFFEA580C),
+          surface: Colors.white,
+        ),
+      ),
     );
   }
 }
