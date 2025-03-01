5f072a115c8c446f8210061a827b00e82d1c1027
yukimiyake0607 <yukimiyake38@gmail.com>
2025-03-01 14:35:51 +0900

    remove: appbarを削除
    - 各pageで設定するため

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:todo/presentation/bottom_navigation/bottom_navigation_provider.dart';
-import 'package:todo/presentation/bottom_navigation/bottom_navigation.dart';
+import 'package:todo/presentation/core/bottom_navigation/bottom_navigation_provider.dart';
+import 'package:todo/presentation/core/bottom_navigation/bottom_navigation.dart';
 import 'package:todo/presentation/pages/completed_page.dart';
 import 'package:todo/presentation/pages/profile_page.dart';
 import 'package:todo/presentation/pages/todo_page.dart';
@@ -13,7 +13,6 @@ class HomePage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final bottomNavigationNum = ref.watch(bottomNavigationProvider);
     return Scaffold(
-      appBar: AppBar(title: const Text('MY TODO')),
       body: IndexedStack(
         index: bottomNavigationNum,
         children: const [
