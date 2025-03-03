0db552e30936618f5322c91f880d8001a9e57858
yukimiyake0607 <yukimiyake38@gmail.com>
2025-03-03 18:43:42 +0900

    update: 改善

--- lib/presentation/core/theme/todo_card_color.dart
+++ lib/presentation/core/theme/todo_card_color.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
 
-const Color todoMainColor = Color(0xFFF97316);
-const Color todoSubColor = Color(0xFFEA580C);
+const todoMainColor = Color(0xFFF97316);
+const todoSubColor = Color(0xFFEA580C);
+const todoCardBorderColor = Color(0xFFFEF3C7);
