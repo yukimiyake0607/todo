50b95dc4058a4cbefdafe65457673605c350dc46
yukimiyake0607 <yukimiyake38@gmail.com>
2025-03-09 21:15:06 +0900

    add: todo_repositoryのインターフェースを追加

--- lib/domain/repositories/todo_repository.dart
+++ lib/domain/repositories/todo_repository.dart
@@ -1,3 +1,24 @@
-// Firebase操作のインターフェスをここで定義
-// 実行するメソッドの内容が決定したら作成
-// 抽象クラス名：ITodoRepository
+import 'package:todo/domain/entities/todo_model.dart';
+
+abstract class ITodoRepository {
+  // すべてのTodoを取得する
+  Stream<List<TodoModel>> getAllTodos();
+
+  // 完了したTodoを取得する
+  Stream<List<TodoModel>> getCompletedTodo();
+
+  // 未完了のTodoを取得する
+  Stream<List<TodoModel>> getInCompletedTodo();
+
+  // Todoを追加する
+  Future<String> addTodo(TodoModel todoModel);
+
+  // Todoを更新する
+  Future<void> updateTodo(TodoModel todoModel);
+
+  // Todoを削除する
+  Future<void> deleteTodo(String id);
+
+  // Todoの完了状態を確認する
+  Future<void> setTodoCompleted(TodoModel todoModel, bool isCompelted);
+}
